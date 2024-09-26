aec72c9cd96b918e79c2fd6edd0cdf65ab51aa50
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2024-09-26 20:40:06 +0530

    Create q1.java

---
+++ basic/basicq/q1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class sum{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter a number:");
+        int num1 = scanner.nextInt();
+        System.out.print("enter a number:");
+        int num2 = scanner.nextInt();
+        int sum = num1 + num2;
+        System.out.print("sum is " + sum);
+        scanner.close();
+    }
+}
