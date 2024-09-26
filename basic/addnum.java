65b211d26ee57556dc3ba053125b86f49a63c3e5
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2024-09-26 13:10:45 +0530

    Create addnum.java

---
+++ basic/addnum.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+        int num1 = 10;//comment
+        int num2 = 11;
+        int sum = num1 + num2;
+        System.out.println(sum);
+        System.out.print(num1 + num2);
+    }
+}
