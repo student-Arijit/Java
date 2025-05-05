765ff87db3249209815fe52051721bc82205dba3
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2025-05-05 18:43:39 +0530

    Create 4vowelorconsonant.java

---
+++ JavaFundamentals/4vowelorconsonant.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main{
+    public static boolean isVowel(char c) {
+        c = Character.toLowerCase(c);
+        return c == 'a' || c == 'e' || c == 'i' || c ==  'o' || c == 'u';
+    }
+
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a Character: ");
+        char ch = sc.next().charAt(0);
+
+        if (isVowel(ch)) {
+            System.out.print("The Character is Vowel.");
+        } else {
+            System.out.print("The Character is consonant.");
+        }
+    }
+}
